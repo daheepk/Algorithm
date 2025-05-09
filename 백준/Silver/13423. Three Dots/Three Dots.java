4940080d800870da5c8f1f49aa9fb340c05e414c
Dahee Park <ekgml3219@gmail.com>
2025-05-09 21:57:28 +0900

    [Silver II] Title: Three Dots, Time: 1092 ms, Memory: 31444 KB -BaekjoonHub

--- 백준/Silver/13423. Three Dots/Three Dots.java
+++ 백준/Silver/13423. Three Dots/Three Dots.java
@@ -3,6 +3,7 @@ import java.lang.*;
 import java.io.*;
 
 class Main {
+    static int[] arr;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -11,28 +12,25 @@ class Main {
         while (T-- > 0){
             int N = Integer.parseInt(br.readLine());
             st = new StringTokenizer(br.readLine());
-            int[] arr = new int[N];
+            arr = new int[N];
+            Map<Integer, Boolean> board = new HashMap<>();
 
             for (int i=0; i<N; i++){
                 arr[i]= Integer.parseInt(st.nextToken());
-            }
-
-            Arrays.sort(arr);
-            
-            Map<Integer, Boolean> board = new HashMap<>();
-            
-            for (int i=0; i<N; i++){
                 board.put(arr[i], true);
             }
 
+            Arrays.sort(arr);
+
             int distance = 0, cnt = 0;
-            
             for (int i=0; i<N-2; i++){
-                for (int j=i+1; j<N-1; j++){
-                    distance = arr[j] - arr[i];
-                    boolean flag = board.getOrDefault(arr[j]+distance, false);
-                    if (flag){
-                        cnt++;
+                for (int j=i+2; j<N; j++){
+                    
+                    if ((arr[j] + arr[i])%2 == 0){
+                        int target = (arr[j] + arr[i])/2;
+                        if (Arrays.binarySearch(arr, i, j, target) >= 0){
+                            cnt++;
+                        }
                     }
                 }
             }
