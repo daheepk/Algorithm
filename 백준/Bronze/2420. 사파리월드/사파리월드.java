d5425a114a6942263e9f217592451807448e8bc8
Dahee Park <ekgml3219@gmail.com>
2025-06-07 21:13:08 +0900

    [Bronze V] Title: 사파리월드, Time: 64 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Integer.parseInt(st.nextToken());
+        long b = Integer.parseInt(st.nextToken());
+
+        System.out.print(Math.abs(a-b));
+    }
+}
