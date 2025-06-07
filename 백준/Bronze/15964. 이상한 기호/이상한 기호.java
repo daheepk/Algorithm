fce600ef7be82102236974c3d126b99117332721
Dahee Park <ekgml3219@gmail.com>
2025-06-08 08:15:24 +0900

    [Bronze V] Title: 이상한 기호, Time: 64 ms, Memory: 11488 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int first = a+b;
+        int last = a-b;
+
+        int result = first * last;
+
+        System.out.print(result);
+    }
+}
