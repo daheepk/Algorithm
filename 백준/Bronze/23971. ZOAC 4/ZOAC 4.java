89dae9e37e8266b2a88b4a778b3bd75c564ead1f
Dahee Park <ekgml3219@gmail.com>
2025-08-12 22:01:14 +0900

    [Bronze III] Title: ZOAC 4, Time: 68 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int H = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int r = (H+N)/(N+1);
+        int c = (W+M)/(M+1);
+
+        System.out.print(r*c);
+    }
+}
